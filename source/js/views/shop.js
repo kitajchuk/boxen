49c468fc29dfbfd8a3cb9aba7f9768c7e0e229da
kitajchuk <bkitajchuk@lazarusnaturals.com>
2020-11-22 15:50:21 -0800

    Sitewide tag system

--- source/js/views/shop.js
+++ source/js/views/shop.js
@@ -1,23 +1,37 @@
 export default ( instance ) => {
     const starred = instance.data.items.find(( item ) => item.starred );
     const items = instance.data.items.filter(( item ) => !item.starred );
+    const collection = instance.data.collection;
+    const tagFilter = instance.data.tagFilter;
     const sysDataVars = "100w,300w,500w,750w,1000w,1500w,2500w";
+
+    // Highlight a tag
+    const hi = ( tag ) => {
+        return (tagFilter && tagFilter === tag ? "hi" : "");
+    };
 
     return `
         <div class="sqs-block-spacer"><div class="sqs-block-content"></div></div>
+        ${tagFilter ? `
+            <div class="shop__filters filters -wrap">
+                <p>Tag filter: <span class="tag pink">${tagFilter}</span></p>
+            </div>
+        ` : ``}
         ${starred ? `
-            <a class="shop__starred starred" href="${starred.fullUrl}">
-                <img class="image js-lazy-image" data-img-src="${starred.assetUrl}" data-variants="${starred.systemDataVariants}" data-original-size="${starred.originalSize}" />
+            <div class="shop__starred starred">
+                <a class="starred__link" href="${starred.fullUrl}">
+                    <img class="image js-lazy-image" data-img-src="${starred.assetUrl}" data-variants="${starred.systemDataVariants}" data-original-size="${starred.originalSize}" />
+                </a>
                 <div class="starred__info">
                     <div class="starred__title" data-content-field="title">${starred.title}</div>
                     <div class="starred__meta">
                         ${starred.tags.map(( tag ) => {
-                            return `<span class="grey">${tag}</span>`;
+                            return `<span class="tag ${hi( tag )}">${hi( tag ) ? tag : `<a href="${collection.fullUrl}?tag=${tag}">${tag}</a>`}</span>`;
 
                         }).join( " " )}
                     </div>
                 </div>
-            </a>
+            </div>
         ` : ``}
         <div class="shop__grid mason ${starred ? `-expt` : ``}">
             ${items.map(( item ) => {
@@ -29,7 +43,7 @@ export default ( instance ) => {
                                 <div class="mason__title">${item.title}${item.structuredContent ? item.structuredContent.variants[ 0 ].qtyInStock === 0 ? ` &mdash; <span class="red">Sold Out</span>` : "" : ""}</div>
                                 <div class="mason__meta">
                                     ${item.tags.map(( tag ) => {
-                                        return `<span class="grey">${tag}</span>`;
+                                        return `<span class="tag ${hi( tag )}">${hi( tag ) ? tag : `<a href="${collection.fullUrl}?tag=${tag}">${tag}</a>`}</span>`;
 
                                     }).join( " " )}
                                 </div>
