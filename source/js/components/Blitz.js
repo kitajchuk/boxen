1549db20d55d651afbdc15f322582052bd669cf8
kitajchuk <bk@kitajchuk.com>
2021-02-06 16:02:24 -0800

    Convert Blitz to our fav -- javascript canvas

--- source/js/components/Blitz.js
+++ source/js/components/Blitz.js
@@ -15,8 +15,8 @@ class Blitz {
             fps: this.fps
         });
 
-        this.make();
         this.bind();
+        this.make();
         this.load().then(() => {
             this.wait();
         });
@@ -25,12 +25,55 @@ class Blitz {
 
     wait () {
         this.blit.blit(( f ) => {
-            this.element[ 0 ].scrollLeft = window.innerWidth * ( f % this.total );
-            // console.log( "FPS:", (f % this.fps), "PNG", (f % this.total) );
+            this.draw( f );
         });
 
         // Fire away!!!
         this.onScroll();
+    }
+
+
+    /*
+    ctx.drawImage(
+        img/cvs,
+        mask-x,
+        mask-y,
+        mask-width,
+        mask-height,
+        x-position,
+        y-position,
+        width,
+        height
+    )
+    */
+    draw ( f ) {
+        // const fps = (f % this.fps);
+        const png = (f % this.total);
+
+        // console.log( "FPS:", fps, "PNG", png );
+
+        this.clear();
+        this.context.drawImage(
+            this.images[ png ],
+            0,
+            0,
+            this.canvas.width,
+            this.canvas.height,
+            0,
+            0,
+            this.canvas.width,
+            this.canvas.height
+        );
+    }
+
+
+    clear () {
+        this.context.clearRect(
+            0,
+            0,
+            this.canvas.width,
+            this.canvas.height
+        );
     }
 
 
@@ -48,11 +91,9 @@ class Blitz {
                 image.onload = () => {
                     idx++;
 
-                    this.aspect.appendChild( image );
                     this.loading.innerHTML = `<p>Sequencing <em>${idx}</em> of <em>${this.total}</em> PNGs</p>`;
 
                     if ( idx === this.total ) {
-                        this.element.append( this.aspect );
                         core.dom.html.removeClass( "is-loader" );
                         resolve();
 
@@ -60,6 +101,8 @@ class Blitz {
                         _load();
                     }
                 };
+
+                this.images[ idx ] = image;
             };
 
             _load();
@@ -76,10 +119,15 @@ class Blitz {
 
     make () {
         this.assets = [];
-        this.aspect = document.createElement( "div" );
+        this.images = [];
+        this.canvas = document.createElement( "canvas" );
+        this.context = this.canvas.getContext( "2d" );
+        this.canvas.width = "1920";
+        this.canvas.height = "1080";
         this.loading = document.createElement( "span" );
         this.loading.innerHTML = `<p>Sequencing ${this.total} PNGs</p>`;
         this.element.append( this.loading );
+        this.element.append( this.canvas );
 
         for ( let i = 1; i <= this.total; i++ ) {
             let idx = i;
@@ -89,8 +137,6 @@ class Blitz {
             }
 
             this.assets.push( `/assets/img/${this.seq}/${this.seq}_${idx}.png` );
-
-
         }
     }
 
