91fa63862018e92c2a0e2c134c4fc94d3bd0f80b
kitajchuk <bk@kitajchuk.com>
2021-02-13 18:43:58 -0800

    Update webpack and eslint, use node-fetch for sqs-reload-trigger

--- webpack.config.js
+++ webpack.config.js
@@ -2,39 +2,70 @@
 const fs = require( "fs" );
 const path = require( "path" );
 const webpack = require( "webpack" );
-const WebpackOnBuildPlugin = require( "on-build-webpack" );
-const request = require( "request" );
 const lager = require( "properjs-lager" );
 const open = require( "open" );
+const fetch = require( "node-fetch" );
 const root = path.resolve( __dirname );
 const source = path.join( root, "source" );
 const nodeModules = "node_modules";
 const config = require( "./boxen.config" );
+const ESLintPlugin = require( "eslint-webpack-plugin" );
+
+
+
+// https://webpack.js.org/contribute/writing-a-plugin/
+// https://webpack.js.org/api/compiler-hooks/
+class BoxenHooksPlugin {
+    constructor ( options ) {
+        this.options = options;
+    }
+
+    apply ( compiler ) {
+        compiler.hooks.afterCompile.tap( "BoxenHooksPlugin", ( compilation ) => {
+            if ( typeof this.options.afterCompile === "function" ) {
+                this.options.afterCompile();
+            }
+        });
+    }
+}
 
 
 
 // Define after config loads
-const plugins = [];
-const pluginOnBuild = new WebpackOnBuildPlugin(() => {
-    // First build opens localhost for you
-    if ( config.open ) {
-        config.open = false;
-        open( `${config.browserUrl}` );
+const plugins = [
+    new ESLintPlugin({
+        emitError: true,
+        emitWarning: false,
+        failOnError: true,
+        quiet: true,
+        context: path.resolve( __dirname, "source" ),
+        exclude: [
+            "node_modules",
+        ],
+    }),
+];
+const pluginHooks = new BoxenHooksPlugin({
+    afterCompile: () => {
+        // First build opens localhost for you
+        if ( config.open ) {
+            config.open = false;
+            open( `${config.browserUrl}` );
 
-    // Subsequent builds trigger SQS reloads
-    } else {
-        request( { url: config.reloadUrl }, () => {
-            lager.server( "sqs local-api reload trigger" );
-        });
-    }
+        // Subsequent builds trigger SQS reloads
+        } else {
+            fetch( config.reloadUrl ).then(() => {
+                lager.server( "sqs local-api reload trigger" );
+            });
+        }
+    },
 });
 
 
 
-module.exports = ( env ) => {
+const webpackConfig = ( env, argv ) => {
     // Only handle builds for sandbox dev environment
     if ( env.sandbox ) {
-        plugins.push( pluginOnBuild );
+        plugins.push( pluginHooks );
     }
 
     return {
@@ -64,18 +95,6 @@ module.exports = ( env ) => {
 
         module: {
             rules: [
-                {
-                    test: /source\/.*\.js$/i,
-                    exclude: /node_modules/,
-                    loader: "eslint-loader",
-                    enforce: "pre",
-                    options: {
-                        emitError: true,
-                        emitWarning: false,
-                        failOnError: true,
-                        quiet: true,
-                    },
-                },
                 {
                     // test: /source\/.*\.js$/i,
                     // exclude: /node_modules/,
@@ -112,3 +131,9 @@ module.exports = ( env ) => {
         }
     };
 };
+
+
+
+module.exports = [
+    webpackConfig,
+];
